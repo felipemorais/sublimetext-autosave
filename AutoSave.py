bfd83b6901e6a485f64af60d9f8c4dfd5737b9f4
Felipe Morais <felipe.morais@hiperativa.com.br>
2013-03-29 01:33:39 -0300

    Added config to save twice to fix bug in liveReload using SASS

--- AutoSave.py
+++ AutoSave.py
@@ -9,19 +9,16 @@ class AutoSaveEventListener(sublime_plugin.EventListener):
 
 	def on_modified(self, view):
 		self.check_save(view)
-		print "modified"
 
 	def check_save(self, view):
 		listSyntax = settings.get('syntaxAutoSave', False)
 		sintax = view.settings().get('syntax').lower()
 		exist = False
 		for item in enumerate(listSyntax):
-			print item[1]
-			print sintax;
-			print sintax.find(item[1])
 			if sintax.find(item[1]) > 0:
 				exist = True
 
 		if (exist and view.file_name() and view.is_dirty() and os.path.exists(view.file_name())):
 			view.run_command('save');
-			print 'save' 
+			if settings.get('saveTwice', False):
+				view.run_command('save');
